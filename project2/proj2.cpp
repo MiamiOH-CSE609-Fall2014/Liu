179bbef248ace348c8b11c498e0ef47faac9f712
liuy23 <liuy23@muohio.edu>
2014-10-12 17:11:56 -0400

    Complete project 2

--- project2/proj2.cpp
+++ project2/proj2.cpp
@@ -7,13 +7,14 @@ using namespace std;
 int main()
 {
 	double * score=NULL;
-	score=new double[10];
+
+	score=new double[100];
 	string * name=NULL;
-	name = new string[10];
+	name = new string[100];
 	string new_name;
 	double new_score;
 	bool test= true;
-	int i=0;
+	int number=0;
 	while(test)
 	{
 		cout<<"Please enter a student's name, or 'none' to quit\n";
@@ -22,9 +23,9 @@ int main()
 		{
 			cout<<"Please enter the score\n"<<endl;
 			cin>>new_score;
-			*(name+i)=new_name;
-			*(score+i)=new_score;					
-			i++;		
+			*(name+number)=new_name;
+			*(score+number)=new_score;					
+			number++;	
 		}
 		else
 		{
@@ -36,16 +37,16 @@ int main()
 
 	double mean = 0.0;
 	double std = 0.0;
-	int number = sizeof(name)-1;
-	cout<<"NUM "<<number;
+	
+//	cout<<"NUM "<<number;
 	double sum=0.0;
 	for (int i=0;i<number;i++)
 	{
 		sum+=score[i];
 	}
-	cout<<"SUM "<<sum;
+//	cout<<"SUM "<<sum;
 	mean=sum/(double)number;
-	cout<<"MEAN "<<mean<<endl;
+//	cout<<"MEAN "<<mean<<endl;
 	double temp=0.0;
 	for (int i=0;i<number;i++)
 	{
@@ -53,6 +54,40 @@ int main()
 	}
 	std=sqrt(temp/number);
 	cout<<"STD "<<std<<endl;
-	
-	
+
+	double c=mean-0.5*std;
+	double b=c+std;
+	double a=b+std;
+	double d=c-std;
+	double f=d-std;
+	cout.precision(1);
+	cout<<"Grading scale\n";
+	if (a>100)
+	{
+	cout<<"A above 100%\n";
+	}
+	else
+	{
+	cout<<"A above "<<fixed<<a<<"%\n";	
+	}	
+	cout<<"B "<<fixed<<b<<"%-"<<a<<"%\n";
+	cout<<"C "<<c<<"%-"<<b<<"%\n";
+	cout<<"D "<<d<<"%-"<<c<<"%\n";
+	cout<<"F "<<f<<"%-"<<d<<"%\n";
+
+	cout<<"Name   Score   Grade\n";
+	for (int i=0;i<number;i++)
+	{
+		cout<<i+1<<". "<<name[i]<<"   "<<score[i]<<"%   ";
+		if(score[i]>=a){cout<<"A\n";}
+		else if(score[i]>=b){cout<<"B\n";}
+		else if(score[i]>=c){cout<<"C\n";}
+		else if(score[i]>=d){cout<<"D\n";}
+		else{ cout<<"F\n";}
+		
+	}
+	cout.precision(2);
+	cout<<"Class average= "<<fixed<<mean<<"%\n";
+	cout<<"Standard Deviation "<<std<<endl;
+	return 0;
 }
