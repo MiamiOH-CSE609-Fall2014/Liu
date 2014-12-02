c0917afd5fa17bc056a9c34067702043e2f3f216
liuy23 <liuy23@muohio.edu>
2014-12-02 18:03:15 -0500

    Updated

--- project4/proj4.cpp
+++ project4/proj4.cpp
@@ -1,22 +1,24 @@
+#include<string.h>
+#include <cstring> 
 #include<stdio.h> 
 #include<stdlib.h> 
-#include<string.h> 
 #include<iostream>
 using namespace std;
 
-void encfile(char *inFile,char *pwd,char *outFile);
-void decryptfile(char *inFile,char *pwd,char *outFile); 
+void encoding(string inFile,string outFile,char *pwd);
+void decoding(string inFile,string outFile,char *pwd); 
+
 
 int main()
 { 
-	int option;
-	char inFile[30];
-	char outFile[30]; 
-	char pwd[8];
+	int c;
+	string inFile;
+	string outFile;
+	char pwd[5];
 
 	cout<<("1. Encrypt a file  2. Decrypt a file\n");	
 	
-	cin>>option;
+	cin>>c;
 	cout<<("Please input In-filename( .txt file):\n"); 
 	cin>>inFile;
 	cout<<("Please input your Password(4 digits number):\n"); 
@@ -24,83 +26,96 @@ int main()
 	cout<<("Please input Out-filename( .txt file):\n"); 
 	cin>>outFile;
 		
-	switch(option){
-	case 1: 
-		cout<<"Input File Name: "<<inFile;
+	if(c==1)
+	{
+		cout<<"Encoding.....\nInput File Name: "<<inFile;
 		cout<<"\nOutput File Name: "<<outFile;
 		cout<<"\nPassword: "<<pwd<<endl;
-		encfile(inFile,pwd,outFile);
-		cout<<"Encrypting Successfully-Remember Your Password Please"<<endl;
-		break;
-	case 2:
-		decryptfile(inFile,pwd,outFile);
-		break;
-	default:
-		break;
-	}	
+		encoding(inFile,outFile,pwd);
+		cout<<"Encoding Successfully"<<endl;
+		
+	}
+	if(c==2)
+	{
+		cout<<"Decoding.....\nInput File Name: "<<inFile;
+		cout<<"\nOutput File Name: "<<outFile;
+		cout<<"\nPassword: "<<pwd<<endl;
+		decoding(inFile,outFile,pwd);
+  		cout<<"Decoding Successfully"<<endl;
+		
+	}
+
 	return 0; 
 } 
 
 
-void encfile(char *inFile,char *pwd,char *out_file) 
+
+void encoding(string fName,string outfName,char *pwd) 
 { 
-	FILE *fp1,*fp2; 
+	FILE *input,*output; 
+	char inFile[30];
+	char out_file[30];
+	strcpy(inFile, fName.c_str());
+	strcpy(out_file, outfName.c_str());
 	register char ch; 
-	int j=0; 
-	fp1=fopen(inFile,"r");
-	if(fp1==NULL){ 
+	int i=0; 
+	input=fopen(inFile,"r");
+	if(input==NULL){ 
 		cout<<("Error Input File Name\n"); 
 		exit(1);
 	} 
-	fp2=fopen(out_file,"w"); 
-	if(fp2==NULL){ 
+	output=fopen(out_file,"w"); 
+	if(output==NULL){ 
 		cout<<("Error Output File Name\n"); 
 		exit(1);
+
 	} 
 
-	while(pwd[++j]);  
+	while(pwd[++i]);  
 
-	ch=fgetc(fp1); 
+	ch=fgetc(input); 
 
 	
-	while(!feof(fp1)){ 
-		if(j >7)
-		   j = 0;
-		ch += pwd[j++];
-		fputc(ch,fp2); 
-		ch=fgetc(fp1); 
+	while(!feof(input)){ 
+		if(i >3)
+		i = 0;
+		ch+=pwd[i++];
+		fputc(ch,output); 
+		ch=fgetc(input); 
 	} 
-	fclose(fp1);
-	fclose(fp2);
+
 } 
 
 
-void decryptfile(char *inFile,char *pwd,char *outFile) 
+void decoding(string fName,string outfName,char *pwd) 
 { 
-	FILE *fp1,*fp2; 
+	char inFile[30];
+	char outFile[30];
+	FILE *input,*output;
+	strcpy(inFile, fName.c_str());
+	strcpy(outFile, outfName.c_str());
 	register char ch; 
-	int j=0; 
-	fp1=fopen(inFile,"r");
-	if(fp1==NULL){ 
+	int i=0; 
+	input=fopen(inFile,"r");
+	if(input==NULL){ 
 		cout<<("Error Input File Name\n"); 
 		exit(1);
 	} 
-	fp2=fopen(outFile,"w"); 
-	if(fp2==NULL){ 
+	output=fopen(outFile,"w"); 
+	if(output==NULL){ 
 		cout<<("Error Output File Name\n"); 
 		exit(1);
 	} 
 	
-	while(pwd[++j]);  	
-	ch=fgetc(fp1); 
+	while(pwd[++i]);  	
+	ch=fgetc(input); 
 	
-	while(!feof(fp1)){ 
-		if(j >7)
-		   j = 0;
-		ch -= pwd[j++];
-		fputc(ch,fp2);
-		ch=fgetc(fp1); 
+	while(!feof(input)){ 
+		if(i >3)
+		i = 0;
+		ch -= pwd[i++];
+		fputc(ch,output);
+		ch=fgetc(input); 
 	} 
-	fclose(fp1);
-	fclose(fp2);
+
 } 
